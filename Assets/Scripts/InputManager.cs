7e0500ff8caf4cc0d058a124ecfc242cd4011d3c
l4auti44 <lautarolamberti.44@gmail.com>
2023-08-08 20:03:03 -0300

    started third person controller

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 public class InputManager : MonoBehaviour
 {
     PlayerControls playerControls;
+    AnimatorManager animatorManager;
 
-   public Vector2 movementInput;
+    public Vector2 movementInput;
+    private float moveAmount;
+    public float verticalInput;
+    public float horizontalInput;
+
+    private void Awake()
+    {
+        animatorManager = GetComponent<AnimatorManager>();
+    }
+
     private void OnEnable()
     {
         if (playerControls == null)
@@ -22,5 +32,20 @@ public class InputManager : MonoBehaviour
     private void OnDisable()
     {
         playerControls.Disable();
+    }
+
+    public void HandleAllInputs()
+    {
+        HandleMovementInput();
+        //HandleJumpingInput
+        //HandleActionInput
+    }
+
+    private void HandleMovementInput()
+    {
+        verticalInput = movementInput.y;
+        horizontalInput = movementInput.x;
+        moveAmount = Mathf.Clamp01(Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
+        animatorManager.UpdateAnimatorValues(0, moveAmount);
     }
 }
--- Assets/Scripts/PlayerLocomotion.cs
+++ Assets/Scripts/PlayerLocomotion.cs
@@ -1,13 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerLocomotion : MonoBehaviour
 {
+    InputManager inputManager;
+
     Vector3 moveDirection;
-    public void HandleMovement()
+    Transform cameraObject;
+    Rigidbody playerRigibody;
+
+    public float movementSpeed = 7;
+    public float rotationSpeed = 15;
+    private void Awake()
     {
-        //moveDirection = cameraOb
+        inputManager = GetComponent<InputManager>();
+        playerRigibody = GetComponent<Rigidbody>();
+        cameraObject = Camera.main.transform;
+    }
+
+    public void HandleAllMovement()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+    private void HandleMovement()
+    {
+        moveDirection = cameraObject.forward * inputManager.verticalInput;
+        moveDirection = moveDirection + cameraObject.right * inputManager.horizontalInput;
+        moveDirection.Normalize();
+        moveDirection.y = 0;
+        moveDirection *= movementSpeed;
+
+        Vector3 movementVelocity = moveDirection;
+        playerRigibody.velocity = movementVelocity;
+
+    }
+
+
+    private void HandleRotation()
+    {
+        Vector3 targetDirection = Vector3.zero;
+
+        targetDirection = cameraObject.forward * inputManager.verticalInput;
+        targetDirection = targetDirection + cameraObject.right * inputManager.horizontalInput;
+        targetDirection.Normalize();
+        targetDirection.y = 0;
+
+        if (targetDirection == Vector3.zero)
+        {
+            targetDirection = transform.forward;
+        }
+
+        quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+        quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        transform.rotation = playerRotation;
     }
         
 }
